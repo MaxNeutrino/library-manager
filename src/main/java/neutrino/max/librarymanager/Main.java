cc05be6254413e87f24181e85e4cd6b4f6db6820
MaxNeutrino <max.stepovoy@gmail.com>
2016-11-10 13:54:06 +0200

    Update Main.java

--- src/main/java/neutrino/max/librarymanager/Main.java
+++ src/main/java/neutrino/max/librarymanager/Main.java
@@ -1,6 +1,7 @@
 package neutrino.max.librarymanager;
 /**
     I used embedded db HSQLDB for local book library
+    Extended - user can set status for book, example: "Thinking in Java - read"
 **/
 
 public class Main {
